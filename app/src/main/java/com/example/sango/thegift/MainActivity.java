8e0eb0e493484e72b8ef3c295c96f296fb371de9
104525006 <104525006@cc.ncu.edu.tw>
2016-04-06 19:41:23 +0800

    gitignore

--- app/src/main/java/com/example/sango/thegift/MainActivity.java
+++ app/src/main/java/com/example/sango/thegift/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //test
+        //testQQ
     }
 
     @Override
